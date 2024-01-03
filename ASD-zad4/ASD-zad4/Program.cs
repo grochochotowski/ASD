d0afe1133bf0c3d958ffc735d1b70cd3fb16ca34
grochochotowski <g.m.rochowski@gmail.com>
2024-01-03 22:38:30 +0100

    Create task 4 template

--- ASD-zad4/ASD-zad4/Program.cs
+++ ASD-zad4/ASD-zad4/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        /// ============================= Variables =============================
+
+
+        /// ============================= Functions =============================
+        
+
+        /// =============================== Main ================================
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creating paths for input and output files
+            string location = @"C:\nonSystem\IT\Code\ASD\ASD-zad4\ASD-zad4\";
+            string pathIn = location + "XX_in_ps7_Grochowski.txt";
+            string pathOut = location + "XX_out_ps7_Grochowski.txt";
+
+            /// ========================= READING INPUT DATA ========================
+
+
+            /// ============================= CALCULATING RESULST =============================
+
+
+
+            /// ============================= SHOWING RESULTS =============================
+
+
+
+            Console.ReadLine(); // Program written by Michał Grochowski
         }
     }
 }
