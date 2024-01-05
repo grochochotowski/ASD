cfb125b402b041470cf0b3064a071017553eabe6
grochochotowski <g.m.rochowski@gmail.com>
2024-01-05 23:11:12 +0100

    Add task content

--- ASD-zad4/ASD-zad4/Program.cs
+++ ASD-zad4/ASD-zad4/Program.cs
@@ -1,5 +1,21 @@
 ﻿namespace ASD_zad4
 {
+    // 1.  Napisz program realizujący książkę telefoniczną w strukturze drzewa AVL
+    //     Program ma umożliwiać wykonanie następujących operacji:
+    //     a)   wstawienie nowego abonenta wraz z numerami telefonów tego abonenta.Porządek
+    //          symetryczny drzewa jest wyznaczony przez porządek leksykograficzny na danych
+    //          abonenta.Dane abonenta to: nazwisko i imię(albo nazwa firmy), adres abonenta.Jeśli
+    //          dwóch abonentów nie rozróżnia nazwisko i imię , to o kolejności danych decyduje adres.
+    //          Zakładamy, że nie ma dwóch abonentów o tym samym imieniu i nazwisku oraz adresie,
+    //     b)   usunięcie abonenta wraz z jego numerami telefonów
+    //     c)   wyszukanie numeru(numerów) telefonu po podaniu danych abonenta(nazwisko i imię
+    //          oraz adres), uwzględnienie komunikatu : Brak abonenta.......
+    //     d)   zapis aktualnej zawartości słownika do pliku(zapisywanie abonentów wraz z danymi).
+    //          Format danych w pliku proszę ustalić samodzielnie.
+    //     e)   odczyt danych z pliku i wstawienie danych do drzewa AVL
+    // 2.  Przygotuj plik do testów – co najmniej 100 abonentów wraz z danymi
+    // 3.  Przetestowanie poprawności zaimplementowanych operacji i ustalenie ich złożoności czasowej
+
     internal class Program
     {
         /// ============================= Variables =============================
