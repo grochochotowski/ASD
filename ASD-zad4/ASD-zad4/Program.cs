bc31e9e272c145f0ea682420b6d3ec9afbe28e5c
grochochotowski <g.m.rochowski@gmail.com>
2024-01-10 19:33:48 +0100

    Change location of data file to use data file instead of temp data

--- ASD-zad4/ASD-zad4/Program.cs
+++ ASD-zad4/ASD-zad4/Program.cs
@@ -290,7 +290,7 @@ namespace ASD_zad4
             
             // Creating paths for input and output files
             string location = @"C:\nonSystem\IT\Code\ASD\ASD-zad4\ASD-zad4\";
-            string data = location + "tempDane.txt";
+            string data = location + "data.txt";
             string book = location + "book.txt";
 
             // Reading data & creating a tree
