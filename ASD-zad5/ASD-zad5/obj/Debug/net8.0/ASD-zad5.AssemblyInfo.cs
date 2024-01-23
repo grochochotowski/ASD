40a0eea3be1a8d4da1155d733b57a9e07111105b
grochochotowski <g.m.rochowski@gmail.com>
2024-01-23 22:06:06 +0100

    Prepare to do dijkstra

--- ASD-zad5/ASD-zad5/obj/Debug/net8.0/ASD-zad5.AssemblyInfo.cs
+++ ASD-zad5/ASD-zad5/obj/Debug/net8.0/ASD-zad5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASD-zad5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+732a456f22d5268bbd25a13b7c01c897b91be21e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac912555a14d8c3f57d1ccce5393c3f38e69250")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASD-zad5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASD-zad5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
